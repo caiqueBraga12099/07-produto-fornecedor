658d9fdc419392434d805c2fd2280fd2417cde4f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-01 11:43:10 -0300

    classe util

---
+++ src/Fornecedor.java
@@ -0,0 +1,27 @@
+
+public class Fornecedor {
+    String nome;
+    private int cnpj;
+
+
+    public Fornecedor(String nome, int cnpj) {
+
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+}
